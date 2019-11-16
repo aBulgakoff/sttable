41370ef7f5734a83d7383d286a76502065711b8d
aBulgakoff <a.p.bulgakoff@gmail.com>
2019-11-16 18:01:06 +0100

    Fixed tests for existing methods.

--- test_parser.py
+++ test_parser.py
@@ -15,9 +15,6 @@ source_table = f'{header}\n{body_row1}\n{body_row2}'
 
 exp_parsed_header = ['header_1st_col', 'header_2nd_col', 'header_3rd_col']
 exp_body = [body_row1, body_row2]
-exp_parsed_obj = {'header_1st_col': ['row_1_of_1st_col', 'row_2_of_1st_col'],
-                  'header_2nd_col': ['row_1_of_2nd_col', 'row_2_of_2nd_col'],
-                  'header_3rd_col': ['row_1_of_3rd_col', 'row_2_of_3rd_col']}
 
 exp_col_0 = ['row_1_of_1st_col', 'row_2_of_1st_col']
 exp_col_1 = ['row_1_of_2nd_col', 'row_2_of_2nd_col']
@@ -26,21 +23,21 @@ exp_cols = [exp_col_0, exp_col_1, exp_col_2]
 exp_get_col = [(0, exp_col_0), (1, exp_col_1), (2, exp_col_2)]
 exp_col_negative_index = (-1, 3, 4)
 
-exp_row_0 = ['row_1_of_1st_col', 'row_1_of_2nd_col', 'row_1_of_3rd_col']
-exp_row_1 = ['row_2_of_1st_col', 'row_2_of_2nd_col', 'row_2_of_3rd_col']
+exp_row_0 = {'header_1st_col': 'row_1_of_1st_col',
+             'header_2nd_col': 'row_1_of_2nd_col',
+             'header_3rd_col': 'row_1_of_3rd_col'}
+exp_row_1 = {'header_1st_col': 'row_2_of_1st_col',
+             'header_2nd_col': 'row_2_of_2nd_col',
+             'header_3rd_col': 'row_2_of_3rd_col'}
 exp_rows = [exp_row_0, exp_row_1]
-exp_get_row = [(0, exp_row_0), (1, exp_row_1)]
-exp_row_negative_index = (-1, 2, 3)
+exp_get_row = [(0, exp_row_0), (1, exp_row_1), (-1, exp_row_1), (-2, exp_row_0)]
+exp_row_negative_index = (2, 3, -3)
 
 
 def test_fields_extracted(tf):
     header_fields = tf(source_table).fields
     assert header_fields == exp_parsed_header, \
         F'Not all fields match. Found {len(header_fields)} field(s) when expected {len(exp_parsed_header)}'
-
-
-def test_container_property(tf):
-    assert tf(source_table).container == exp_parsed_obj, f'Objects do not match.'
 
 
 @pytest.mark.parametrize('index, expected_column', exp_get_col)
@@ -56,12 +53,12 @@ def test_try_get_nonexistent_column(tf, index):
 
 @pytest.mark.parametrize('index, expected_row', exp_get_row)
 def test_get_row(tf, index, expected_row):
-    assert tf(source_table).row[index] == expected_row
+    assert tf(source_table).row(index) == expected_row
 
 
 @pytest.mark.parametrize('index', exp_row_negative_index)
 def test_get_nonexistent_row(tf, index):
-    with pytest.raises(ValueError):
+    with pytest.raises(IndexError):
         tf(source_table).row(index)
 
 
