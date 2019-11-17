d84abbcf8741400bd6a02a076092b322ee9eee44
aBulgakoff <a.p.bulgakoff@gmail.com>
2019-11-17 08:47:14 +0100

    Added Test for empty value in middle of Row.

--- test_parser.py
+++ test_parser.py
@@ -10,8 +10,10 @@ Input:
 HEADER = '| header_1st_col | header_2nd_col | header_3rd_col |'
 BODY_ROW1 = '| row_1_of_1st_col | row_1_of_2nd_col | row_1_of_3rd_col |'
 BODY_ROW2 = '| row_2_of_1st_col | row_2_of_2nd_col | row_2_of_3rd_col |'
+BODY_ROW2_MISSING_MID = '| row_2_of_1st_col |   | row_2_of_3rd_col |'
 
 SOURCE_TABLE = f'{HEADER}\n{BODY_ROW1}\n{BODY_ROW2}'
+SOURCE_TABLE_MISSING_REC = f'{HEADER}\n{BODY_ROW1}\n{BODY_ROW2_MISSING_MID}'
 
 EXP_FIELDS = ['header_1st_col', 'header_2nd_col', 'header_3rd_col']
 EXP_BODY = [BODY_ROW1, BODY_ROW2]
@@ -23,7 +25,7 @@ EXP_COL2 = ['row_1_of_3rd_col', 'row_2_of_3rd_col']
 EXP_COLS = {EXP_FIELDS[0]: EXP_COL0, EXP_FIELDS[1]: EXP_COL1, EXP_FIELDS[2]: EXP_COL2}
 
 EXP_GET_COL = [(0, EXP_COL0), (1, EXP_COL1), (2, EXP_COL2)]
-EXP_NONEXIST_COL_IX = (-4, 3, 4)
+EXP_NONEXISTENT_COL_INDEX = (-4, 3, 4)
 
 EXP_ROW0 = {'header_1st_col': 'row_1_of_1st_col',
             'header_2nd_col': 'row_1_of_2nd_col',
@@ -31,9 +33,12 @@ EXP_ROW0 = {'header_1st_col': 'row_1_of_1st_col',
 EXP_ROW1 = {'header_1st_col': 'row_2_of_1st_col',
             'header_2nd_col': 'row_2_of_2nd_col',
             'header_3rd_col': 'row_2_of_3rd_col'}
+EXP_ROW1_MISSING_REC = {'header_1st_col': 'row_2_of_1st_col',
+                        'header_2nd_col': '',
+                        'header_3rd_col': 'row_2_of_3rd_col'}
 EXP_ROWS = [EXP_ROW0, EXP_ROW1]
 EXP_GET_ROW = [(0, EXP_ROW0), (1, EXP_ROW1), (-1, EXP_ROW1), (-2, EXP_ROW0)]
-EXP_NONEXIST_ROW_IX = (2, 3, -3)
+EXP_NONEXISTENT_ROW_INDEX = (2, 3, -3)
 
 
 def test_fields_extracted(tf):
@@ -47,7 +52,7 @@ def test_get_column(tf, index, expected_column):
     assert tf(SOURCE_TABLE).get_column(index) == expected_column
 
 
-@pytest.mark.parametrize('index', EXP_NONEXIST_COL_IX)
+@pytest.mark.parametrize('index', EXP_NONEXISTENT_COL_INDEX)
 def test_try_get_nonexistent_column(tf, index):
     with pytest.raises(IndexError):
         tf(SOURCE_TABLE).get_column(index)
@@ -62,7 +67,11 @@ def test_get_row(tf, index, expected_row):
     assert tf(SOURCE_TABLE).get_row(index) == expected_row
 
 
-@pytest.mark.parametrize('index', EXP_NONEXIST_ROW_IX)
+def test_get_row_missing_rec(tf):
+    assert tf(SOURCE_TABLE_MISSING_REC).get_row(1) == EXP_ROW1_MISSING_REC
+
+
+@pytest.mark.parametrize('index', EXP_NONEXISTENT_ROW_INDEX)
 def test_get_nonexistent_row(tf, index):
     with pytest.raises(IndexError):
         tf(SOURCE_TABLE).get_row(index)
